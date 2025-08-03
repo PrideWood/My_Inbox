7de26bdc2304ac317ea7b2958d8ce17fe6dccf05
Wesley <boquanchai@gmail.com>
2025-08-03 22:52:45 +0800

    initial commit

---
+++ quartz/components/pages/404.tsx
@@ -0,0 +1,31 @@
+import { i18n } from "../../i18n"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
+
+const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
+  // If baseUrl contains a pathname after the domain, use this as the home link
+  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+  const baseDir = url.pathname
+
+  return (
+    <article class="popover-hint">
+      <h1>404</h1>
+
+      {/* 添加 SVG 插图 */}
+      <img
+        src="static/404-illustration.svg"
+        alt="Not found illustration"
+        style={{
+          maxWidth: "200px",
+          width: "100%",
+          margin: "2rem 0",
+          display: "block"
+        }}
+      />
+
+      <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
+    </article>
+  )
+}
+
+export default (() => NotFound) satisfies QuartzComponentConstructor
